f9807fba7deb28fce2824052e97a3e8508bb1964
timo95 <24251362+timo95@users.noreply.github.com>
2024-01-07 14:33:53 +0100

    Abstract storage mechanism

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,4 @@
+import {deleteValue, getValue, setValue} from "./storage";
 
 const DEFAULT_STASH_URL = "http://localhost:9999"
 const BLOCKED_SITE_KEY = `blocked_${window.location.host}`.replace(/[.\-]/, "_");
@@ -65,49 +66,49 @@ function initEndpoints(): HTMLDivElement {
 }
 
 export async function isSiteBlocked(): Promise<boolean> {
-    return await GM.getValue(BLOCKED_SITE_KEY, false);
+    return await getValue(BLOCKED_SITE_KEY, false);
 }
 
 async function blockSite() {
-    await GM.setValue(BLOCKED_SITE_KEY, true);
+    await setValue(BLOCKED_SITE_KEY, true);
     window.location.reload();
 }
 
 async function unblockSite() {
-    await GM.deleteValue(BLOCKED_SITE_KEY);
+    await deleteValue(BLOCKED_SITE_KEY);
     window.location.reload();
 }
 
 async function setStashUrl() {
-    let stashUrl = await GM.getValue<string>("stashUrl", undefined);
+    let stashUrl = await getValue<string>("stashUrl", undefined);
     stashUrl = prompt("Stash URL:", stashUrl ?? DEFAULT_STASH_URL)?.trim()?.replace("\/$", "")
     if (stashUrl !== undefined) {
-        await GM.setValue("stashUrl", stashUrl)
+        await setValue("stashUrl", stashUrl)
     }
 }
 
 async function setApiKey() {
-    let apiKey = await GM.getValue<string>("apiKey", undefined);
+    let apiKey = await getValue<string>("apiKey", undefined);
     apiKey = prompt("API Key:", apiKey ?? "")?.trim()?.replace("\/$", "")
     if (apiKey !== undefined) {
-        await GM.setValue("apiKey", apiKey)
+        await setValue("apiKey", apiKey)
     }
 }
 
 export async function getConfig(): Promise<[string, string]> {
-    let stashUrl = await GM.getValue<string>("stashUrl", undefined);
-    let apiKey = await GM.getValue<string>("apiKey", undefined);
+    let stashUrl = await getValue<string>("stashUrl", undefined);
+    let apiKey = await getValue<string>("apiKey", undefined);
 
     if (stashUrl === undefined) {
         stashUrl = prompt("Stash URL:", DEFAULT_STASH_URL)?.trim()?.replace("\/$", "")
         if (stashUrl !== undefined) {
-            await GM.setValue("stashUrl", stashUrl)
+            await setValue("stashUrl", stashUrl)
         }
     }
     if (apiKey === undefined) {
         apiKey = prompt("API Key:")?.trim()?.replace("\/$", "")
         if (apiKey !== undefined) {
-            await GM.setValue("apiKey", apiKey)
+            await setValue("apiKey", apiKey)
         }
     }
 
---
+++ src/storage.ts
@@ -0,0 +1,39 @@
+
+/**
+ * Get value of type T from storage.
+ * Implementation of storage differs between userscript and browser extension.
+ *
+ * @param key key used to store value
+ * @param defaultValue optional default value
+ */
+export async function getValue<T>(key: string, defaultValue?: T): Promise<T> {
+    const text = await GM.getValue<string>(key, undefined);
+    try {
+        return text === undefined ? Promise.resolve(defaultValue) : JSON.parse(text) as T;
+    } catch (e: any) {
+        console.warn("Failed to parse stored value. Delete stored key-value pair.")
+        await deleteValue(key);
+        return defaultValue;
+    }
+}
+
+/**
+ * Write value of type T to storage.
+ * Implementation of storage differs between userscript and browser extension.
+ *
+ * @param key key used to store value
+ * @param value value to be stored
+ */
+export async function setValue<T>(key: string, value: T): Promise<void> {
+    return GM.setValue(key, JSON.stringify(value));
+}
+
+/**
+ * Delete key-value pair from storage.
+ * Implementation of storage differs between userscript and browser extension.
+ *
+ * @param key key used to store value
+ */
+export async function deleteValue(key: string): Promise<void> {
+    return GM.deleteValue(key);
+}
